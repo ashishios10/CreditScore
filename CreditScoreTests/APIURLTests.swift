6392e939e639651b321c47a349bbd74f1be237ec
Ashish Patel <ashish.ios10@gmail.com>
2024-09-12 01:18:45 +0530

    Credit score sample code

---
+++ CreditScoreTests/APIURLTests.swift
@@ -0,0 +1,28 @@
+//
+//  APIURLTests.swift
+//  CreditScoreTests
+//
+//  Created by Ashish Patel on 12/28/22.
+//
+
+import XCTest
+import Networking
+
+@testable import CreditScore
+
+class APIURLTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        super.setUp()
+    }
+    
+    override func tearDownWithError() throws {
+        super.tearDown()
+    }
+    
+    func testGetCreditScoreURL() throws {
+        let url = APIURL.getCreditScore.getURL()
+        XCTAssertEqual(url, "https://5lfoiyb0b3.execute-api.us-west-2.amazonaws.com/prod/mockcredit/values")
+    }
+    
+}
